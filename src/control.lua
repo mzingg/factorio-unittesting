86b8e3a119b6e69cb9df3943b778f28cfcbe4d25
Markus Zingg <markus.zingg@gmail.com>
2017-07-09 11:55:46 +0200

    adds support for creating entities by robots

--- src/control.lua
+++ src/control.lua
@@ -6,6 +6,10 @@ script.on_event(defines.events.on_built_entity, function(eventArguments)
     UnitTesting.Monitor:register(eventArguments.created_entity)
 end)
 
+script.on_event(defines.events.on_robot_built_entity, function(eventArguments)
+    UnitTesting.Monitor:register(eventArguments.created_entity)
+end)
+
 script.on_event(defines.events.on_entity_died, function(eventArguments)
     UnitTesting.Monitor:unregister(eventArguments.entity)
 end)
